b0e8aef436618da4ea9b9358cc29e964d11f926f
sjames <suzannahsmjames1@gmail.com>
2018-02-15 17:18:33 +0000

    angular part II: the beginning of the end

--- src/app/entries/entry-list/entry-list.component.ts
+++ src/app/entries/entry-list/entry-list.component.ts
@@ -1,4 +1,7 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { EntryService } from ',./shared/entry.service';
+import { Entry } from '../shared/entry.model';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-entry-list',
@@ -6,6 +9,15 @@ import { Component} from '@angular/core';
   styleUrls: ['entry-list.component.css']
 })
 
-export class EntryListComponent {
-  
+export class EntryListComponent implements OnInit {
+  entries: Entry[];
+  constructor(private entryService: EntryService) {
+
+  }
+
+  ngOnInit() {
+    this.entryService
+      .getEntries()
+      .then(entries => this.entries = entries);
+  }
 }
